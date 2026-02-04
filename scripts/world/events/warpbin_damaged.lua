31cb2aa38587dcc4bc4b0d867ec7b7fe96f075c3
Nell <alex.cimer@mail.ru>
2026-02-05 01:02:17 +0300

    Make damaged WarpBin into a separate event

--- scripts/world/events/warpbin_damaged.lua
+++ scripts/world/events/warpbin_damaged.lua
@@ -7,11 +7,7 @@ function WarpBin:init(data)
 
     self.properties = data.properties or {}
 
-    if self.properties["skin"] then
-        self.sprite_b = Sprite("world/events/".. self.properties["skin"])
-    else
-        self.sprite_b = Sprite("world/events/damaged_warp_bin")
-    end
+    self.sprite_b = Sprite("world/events/damaged_warp_bin")
 
     self.sprite_b:setScale(2)
     self.sprite_b.debug_select = false
