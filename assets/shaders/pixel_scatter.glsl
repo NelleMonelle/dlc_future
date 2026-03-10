2fc47c80ad1b12a3cc76e03d1b6510e3473d8d1a
AcousticJamm <jasmicheals@gmail.com>
2026-03-10 13:14:51 -0400

    Shader fixes

--- assets/shaders/pixel_scatter.glsl
+++ assets/shaders/pixel_scatter.glsl
@@ -16,8 +16,8 @@ vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
     vec2 coords_floored = vec2(floor_multiple(texture_coords.x - screen_pos.x, pixel_size / texture_size.x),
                                floor_multiple(texture_coords.y - screen_pos.y, pixel_size / texture_size.y));
-    float x_offset = (random(seed * coords_floored      ) - 0.5) * 2 * max_dist / texture_size.x;
-    float y_offset = (random(seed * coords_floored * 1.1) - 0.5) * 2 * max_dist / texture_size.y;
+    float x_offset = (random(seed * coords_floored      ) - 0.5) * 2.0 * max_dist / texture_size.x;
+    float y_offset = (random(seed * coords_floored * 1.1) - 0.5) * 2.0 * max_dist / texture_size.y;
 
     return Texel(texture, vec2(texture_coords.x + x_offset, texture_coords.y + y_offset)) * color;
 }
