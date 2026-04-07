968d89474096c31c01a8a4348318f76ab5116c34
AcousticJamm <jasmicheals@gmail.com>
2026-04-06 22:09:38 -0400

    Achievements! Finally!

---
+++ achievements.lua
@@ -0,0 +1,60 @@
+local achievements = {
+	-- {
+		-- id = "ending",
+		-- name = "Dark Place: The Dark Future",
+		-- desc = "Complete the Dark Future DLC.",
+		-- hint = "Complete the Dark Future DLC."
+	-- },
+	
+	{
+		id = "chase_fparty_nohit",
+		name = "No-Hit: Future Party Chase",
+		desc = "Complete the Future Party chase sequence, including the Future Marcy encounter, without getting hit.",
+		hint = "Complete something without getting hit.",
+		border = "NoHit",
+		icon = "fp_chase",
+		hidden = true
+	},
+	
+	{
+		id = "chase_knight_nohit",
+		name = "No-Hit: Knight Chase",
+		desc = "Complete the Knight chase sequence without getting hit.",
+		hint = "Complete something without getting hit.",
+		border = "NoHit",
+		icon = "knight_chase",
+		hidden = true
+	},
+	
+	{
+		id = "reputation_good",
+		name = "A Good Samaritan",
+		desc = "Get 100% positive reputation.",
+		hint = "Get 100% positive reputation.",
+		border = "Unique"
+	},
+	
+	{
+		id = "reputation_bad",
+		name = "Get lost, you freak!",
+		desc = "Get 100% negative reputation.",
+		hint = "Get 100% negative reputation.",
+		border = "Common"
+	},
+	
+	-- {
+		-- id = "ending_good",
+		-- name = "Our Two Homes",
+		-- desc = "Spare Future Noelle in the final battle.",
+		-- hint = "Spare ???."
+	-- },
+	
+	-- {
+		-- id = "ending_bad",
+		-- name = "Won't Affect Me",
+		-- desc = "Kill Future Noelle in the final battle.",
+		-- hint = "Kill ???."
+	-- },
+}
+
+return achievements
