05f6a0e0eb33fd64812af4965260f758d53efa87
AcousticJamm <jasmicheals@gmail.com>
2026-05-15 21:36:40 -0400

    TitanSpawn no longer levels up F!Party
    workaround

--- libraries/titan_spawn_lib/scripts/battle/enemies/titan_spawn.lua
+++ libraries/titan_spawn_lib/scripts/battle/enemies/titan_spawn.lua
@@ -426,6 +426,25 @@ function TitanSpawn:onDefeat(damage, battler)
     self:onDefeatFatal(damage, battler)
 end
 
+function TitanSpawn:onDefeatFatal(damage, battler)
+    self.hurt_timer = -1
+
+    Assets.playSound("deathnoise")
+
+    local sprite = self:getActiveSprite()
+
+    sprite.visible = false
+    sprite:stopShake()
+
+    local death_x, death_y = sprite:getRelativePos(0, 0, self)
+    local death = FatalEffect(sprite:getTexture(), death_x, death_y, function() self:remove() end)
+    death:setColor(sprite:getDrawColor())
+    death:setScale(sprite:getScale())
+    self:addChild(death)
+
+    self:defeat("VIOLENCED", true)	-- no LV up
+end
+
 function TitanSpawn:freeze()
     self:onDefeat()
 end
