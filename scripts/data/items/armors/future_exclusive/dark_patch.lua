db169f3b02e509c204042f67caa301d5515ccb34
AcousticJamm <jasmicheals@gmail.com>
2026-05-22 14:40:10 -0400

    some new armors

---
+++ scripts/data/items/armors/future_exclusive/dark_patch.lua
@@ -0,0 +1,56 @@
+local item, super = Class(Item, "dark_patch")
+
+function item:init()
+    super.init(self)
+
+    -- Display name
+    self.name = "DarkPatch"
+
+    -- Item type (item, key, weapon, armor)
+    self.type = "armor"
+    -- Item icon (for equipment)
+    self.icon = "ui/menu/icon/armor"
+
+    -- Battle description
+    self.effect = ""
+    -- Shop description
+    self.shop = ""
+    -- Menu description
+    self.description = "An eyepatch with darkness radiating.\nWearer deals and takes more DARK damage."
+
+    -- Shop buy price
+    self.buy_price = 0
+    -- Shop sell price (set to nil)
+    self.sell_price = nil
+
+    -- Consumable target mode (party, enemy, noselect, or none/nil)
+    self.target = nil
+    -- Where this item can be used (world, battle, all, or none/nil)
+    self.usable_in = "all"
+    -- Item this item will get turned into when consumed
+    self.result_item = nil
+    -- Will this item be instantly consumed in battles?
+    self.instant = false
+
+    -- Equip bonuses (for weapons and armor)
+    self.bonuses = {
+        defense = 12,
+    }
+	
+    -- Bonus name and icon (displayed in equip menu)
+    self.bonus_name = nil
+    self.bonus_icon = nil
+
+    -- Equippable characters (default true for armors, false for weapons)
+    self.can_equip = {
+		jamm = false
+	}
+
+    -- Character reactions
+    self.reactions = {
+        jamm = "It's kinda tight...",
+        fmarcy = "I needed a new one anyways.",
+	}
+end
+
+return item
