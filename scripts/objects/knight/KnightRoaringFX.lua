0c03a8427c3fb9f91a135b47e2a7cbb3cdd9d276
Diamond Deltahedron <183671108+DIAMONDDELTAHEDRON@users.noreply.github.com>
2026-05-23 12:52:11 -0700

    Update KnightRoaringFX.lua

--- scripts/objects/knight/KnightRoaringFX.lua
+++ scripts/objects/knight/KnightRoaringFX.lua
@@ -36,7 +36,7 @@ function KnightRoaringFX:init(x, y)
     self.highlight:setColor(1, 1, 1)
     self.highlight.amount = 0
 	self.intro_con = 0
-	self.attack_con = 0
+	self.attack_con = -1
 end
 
 function KnightRoaringFX:update()
@@ -108,9 +108,6 @@ function KnightRoaringFX:update()
 			self.intro_con = self.intro_con + 1
         end
         if self.timer >= 64 and self.intro_con == 4 then
-			if Game.sepia_shader and Game.sepia_shader.active then
-				Game.world:addChild(FakeSepiaManager())
-			end
             self.state = "roaring"
             self.timer = -20
 			self.intro_con = 5
@@ -120,6 +117,12 @@ function KnightRoaringFX:update()
     if self.state == "roaring" then
         self.timer = self.timer + DTMULT
 		
+		if self.timer >= 0 and self.attack_con == -1 then
+			if Game.sepia_shader and Game.sepia_shader.active then
+				Game.world:addChild(FakeSepiaManager())
+			end
+			self.attack_con = 0
+		end
         if self.timer >= 16 and self.attack_speed == 0 and self.attack_con == 0 then
             self.bar = 24
 			self.attack_con = 1
